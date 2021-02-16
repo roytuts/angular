f3bfc37e5a477b45467685bb919e8f1e735fcea8
Soumitra <email@email.com>
2021-02-16 17:12:06 +0530

    updated for angular 11

--- angular-file-download/app/app.component.ts
+++ angular-file-download/app/app.component.ts
@@ -13,13 +13,15 @@ export class AppComponent {
   constructor(private fileService: FileService) {}
   
   download() {
-    this.fileService.downloadFile().subscribe(response => {
+    //this.fileService.downloadFile().subscribe(response => {
+		this.fileService.downloadFile().subscribe((response: any) => { //when you use stricter type checking
 			let blob:any = new Blob([response], { type: 'text/json; charset=utf-8' });
 			const url = window.URL.createObjectURL(blob);
 			//window.open(url);
 			//window.location.href = response.url;
 			fileSaver.saveAs(blob, 'employees.json');
-		}), error => console.log('Error downloading the file'),
+		//}), error => console.log('Error downloading the file'),
+		}), (error: any) => console.log('Error downloading the file'), //when you use stricter type checking
                  () => console.info('File downloaded successfully');
   }
   
