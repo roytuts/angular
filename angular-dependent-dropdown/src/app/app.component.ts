c053c40852f51d4885f80a68bfa09fcdcf670127
Soumitra <email@email.com>
2021-02-23 13:54:20 +0530

    updated for angular 8 and 11

--- angular-dependent-dropdown/src/app/app.component.ts
+++ angular-dependent-dropdown/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { OnInit, Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -24,15 +24,22 @@ export class AppComponent implements OnInit {
 		{ name: 'India', states: [ {name: 'E', cities: ['Delhi', 'Kolkata', 'Mumbai', 'Bangalore']} ] },
 	];
   
-	states: Array<any>;
+    //states: Array<any>; //Angular 8
+	states: Array<any> = []; //Angular 11
 
-	cities: Array<any>;
+	//cities: Array<any>; //Angular 8
+	cities: Array<any> = []; //Angular 11
 	
-	changeCountry(country) {
-		this.states = this.Countries.find(cntry => cntry.name == country).states;
+	//changeCountry(country) { //Angular 8
+	changeCountry(country: any) { //Angular 11
+		//this.states = this.Countries.find(cntry => cntry.name == country).states; //Angular 8
+		this.states = this.Countries.find((cntry: any) => cntry.name == country.target.value).states; //Angular 11
 	}
 
-	changeState(state) {
-		this.cities = this.Countries.find(cntry => cntry.name == this.selectedCountry).states.find(stat => stat.name == state).cities;
+	//changeState(state) { //Angular 8
+	changeState(state: any) { //Angular 11
+		//this.cities = this.Countries.find(cntry => cntry.name == this.selectedCountry).states.find(stat => stat.name == state).cities; //Angular 8
+		this.cities = this.Countries.find((cntry: any) => cntry.name == this.selectedCountry).states.find((stat: any) => stat.name == state.target.value).cities; //Angular 11
 	}
+	
 }
--- angular-dependent-dropdown/src/app/app.module.ts
+++ angular-dependent-dropdown/src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,8 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
 	FormsModule,
+    BrowserModule,
     AppRoutingModule
   ],
   providers: [],
