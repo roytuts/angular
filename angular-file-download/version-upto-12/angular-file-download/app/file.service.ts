cd3157228d26b8b9e091ef42c2bff1170fa0727e
roytuts <projwebinfo@gmail.com>
2021-11-17 08:13:28 +0530

    updated for angular version 13

---
+++ angular-file-download/version-upto-12/angular-file-download/app/file.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+//import {Http, ResponseContentType} from '@angular/http';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class FileService {
+
+  constructor(private http: HttpClient) {}
+
+//downloadFile(): Observable<any> {
+		//return this.http.get('http://localhost:8080/employees/download', {responseType: ResponseContentType.Blob});
+  downloadFile(): any {
+		return this.http.get('http://localhost:8080/employees/download', {responseType: 'blob'});
+  }
+   
+}
