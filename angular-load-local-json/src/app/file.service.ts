d74a95d60899e766a4d6576ff8f203b5f537b6ba
Soumitra <projwebinfo@gmail.com>
2022-08-19 18:20:45 +0530

    Update file.service.ts

--- angular-load-local-json/src/app/file.service.ts
+++ angular-load-local-json/src/app/file.service.ts
@@ -9,7 +9,12 @@ export class FileService {
 	constructor(private http: HttpClient) { }
 
 	getResultList() {
+		const currentLocation = window.location.origin;
 		const headers = new HttpHeaders({ 'Content-Type': 'application/json' })
-        return this.http.get('./assets/data/results.json', { headers });
+		
+        //return this.http.get('./assets/data/results.json', { headers });
+		
+		return this.http.get(currentLocation + '/assets/data/results.json', { headers });
     }
+	
 }
