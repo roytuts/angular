47482bf04d3bc1e8e5861f00ffb7c9148f7fddff
Soumitra <projwebinfo@gmail.com>
2022-12-13 15:44:11 +0530

    Create file.service.ts

---
+++ angular-file-download/version-15/angular-file-download/src/app/file.service.ts
@@ -0,0 +1,14 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class FileService {
+
+	constructor(private http: HttpClient) {}
+
+	downloadFile(): any {		
+		return this.http.get('http://localhost:8080/employees/download', {responseType: 'blob'});
+	}
+   
+}
