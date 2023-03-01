9fd8bc5bf44634a988c279af6b60a73f8030445a
Soumitra <projwebinfo@gmail.com>
2023-03-01 13:48:56 +0530

    Create app.component.ts

---
+++ angular-prevent-new-line-text-box/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular Prevent New Line Text Box';
+  
+  onEnterKeyPress(event: any){
+      event.preventDefault();
+  }
+}
