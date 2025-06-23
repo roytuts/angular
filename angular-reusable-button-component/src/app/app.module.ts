35a44677172703fe5c4489e14d4d0acaca373127
Soumitra <projwebinfo@gmail.com>
2025-06-23 09:03:05 +0530

    Create app.module.ts

---
+++ angular-reusable-button-component/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ButtonComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
