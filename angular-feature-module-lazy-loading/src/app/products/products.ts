d121740265ddde6bebce018944c2ffc5b40eeb19
Soumitra <projwebinfo@gmail.com>
2025-07-24 15:17:19 +0530

    Create products.ts

---
+++ angular-feature-module-lazy-loading/src/app/products/products.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  standalone: false,
+  templateUrl: './products.html',
+  styleUrl: './products.css'
+})
+export class Products {
+
+}
