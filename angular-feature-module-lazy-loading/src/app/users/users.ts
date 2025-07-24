dc1de4fcad803248cd74b89fd5fd1c48dc1a7bb9
Soumitra <projwebinfo@gmail.com>
2025-07-24 15:13:59 +0530

    Create users.ts

---
+++ angular-feature-module-lazy-loading/src/app/users/users.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  standalone: false,
+  templateUrl: './users.html',
+  styleUrl: './users.css'
+})
+export class Users {
+
+}
