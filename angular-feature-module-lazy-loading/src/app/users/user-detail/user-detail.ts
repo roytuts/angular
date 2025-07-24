47bd13838c19af95b1b7f2a03416d1e2f4dda3f8
Soumitra <projwebinfo@gmail.com>
2025-07-24 15:16:06 +0530

    Create user-detail.ts

---
+++ angular-feature-module-lazy-loading/src/app/users/user-detail/user-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-detail',
+  standalone: false,
+  templateUrl: './user-detail.html',
+  styleUrl: './user-detail.css'
+})
+export class UserDetail {
+
+}
