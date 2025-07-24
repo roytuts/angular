98b037b9776a201930fa5d3bd064ab829fe66cf6
Soumitra <projwebinfo@gmail.com>
2025-07-24 15:19:32 +0530

    Create product-detail.ts

---
+++ angular-feature-module-lazy-loading/src/app/products/product-detail/product-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-detail',
+  standalone: false,
+  templateUrl: './product-detail.html',
+  styleUrl: './product-detail.css'
+})
+export class ProductDetail {
+
+}
