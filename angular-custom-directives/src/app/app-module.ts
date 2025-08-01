b12552c8b7c253b8d374ab95075e9b2e3c156486
Soumitra <projwebinfo@gmail.com>
2025-08-01 14:52:25 +0530

    Create app-module.ts

---
+++ angular-custom-directives/src/app/app-module.ts
@@ -0,0 +1,24 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { HighlightDirective } from './highlight';
+import { TooltipDirective } from './tooltip';
+
+@NgModule({
+  declarations: [
+    App,
+    HighlightDirective,
+    TooltipDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
