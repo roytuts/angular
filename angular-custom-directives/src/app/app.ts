76fd7e13fbb80703999384b4d42a2885c0f9fe37
Soumitra <projwebinfo@gmail.com>
2025-08-01 14:51:45 +0530

    Create app.ts

---
+++ angular-custom-directives/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  standalone: false,
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Angular Custom Directives');
+  
+  color = '';
+  
+}
