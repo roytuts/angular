647de03e136b1d57f245789a5d1245cebb0d495d
Soumitra <projwebinfo@gmail.com>
2025-08-08 15:46:28 +0530

    Create app-module.ts

---
+++ angular-reactive-forms/src/app/app-module.ts
@@ -0,0 +1,24 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+
+@NgModule({
+  declarations: [
+    App
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+	ReactiveFormsModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
