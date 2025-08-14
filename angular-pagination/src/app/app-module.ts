7b90d1be62eed7cd15c3a436e83541a1b8e76d8b
Soumitra <projwebinfo@gmail.com>
2025-08-14 12:45:27 +0530

    Create app-module.ts

---
+++ angular-pagination/src/app/app-module.ts
@@ -0,0 +1,24 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { provideHttpClient, withFetch } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+
+@NgModule({
+  declarations: [
+    App
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideClientHydration(withEventReplay()),
+	provideHttpClient(withFetch())
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
